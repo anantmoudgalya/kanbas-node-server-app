23af0af5063f14bffa0037a890c9edd7cde14e99
Anant Moudgalya <anant9798@gmail.com>
2023-11-29 00:20:49 -0800

    - done until 3.5.2

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import UserRoutes from "./users/routes.js";
 import "dotenv/config";
+
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
 
@@ -46,6 +50,7 @@ app.use(express.json());
 
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 
